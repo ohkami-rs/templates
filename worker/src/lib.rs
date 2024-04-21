30d1ba2ce02b1c871491bbe1b4b28bbfa0b8e9e6
kana-rus <kanarus786@gmail.com>
2024-04-21 21:35:53 +0900

    Add `#[cfg(feature = "DEBUG")] console_error_panic_hook::set_once();` & Update env.dev.build`

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -2,6 +2,9 @@ use ohkami::prelude::*;
 
 #[ohkami::worker]
 async fn my_worker() -> Ohkami {
+    #[cfg(feature = "DEBUG")]
+    console_error_panic_hook::set_once();
+
     Ohkami::new((
         "/".GET(|| async {"Hello, world!"}),
     ))
