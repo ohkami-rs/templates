528395361671975ae691bbb88fa66e7261037e64
kana-rus <kanarus786@gmail.com>
2024-06-12 18:43:23 +0900

    `errors::ServerError`: add `Worker(#[from] worker::Error)` variant by default

--- worker_yew_spa/src/api/errors.rs
+++ worker_yew_spa/src/api/errors.rs
@@ -3,10 +3,16 @@ use ohkami::prelude::*;
 
 #[derive(Debug, thiserror::Error)]
 pub enum ServerError {
+    #[error("Worker error: {0}")]
+    Worker(#[from] worker::Error),
 }
 
 impl IntoResponse for ServerError {
     fn into_response(self) -> Response {
-        todo!()
+        worker::console_error!("{self}");
+
+        match self {
+            Self::Worker(_) => Response::InternalServerError(),
+        }
     }
 }
